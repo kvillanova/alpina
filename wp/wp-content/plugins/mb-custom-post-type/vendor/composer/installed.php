3791c5589c875db662c1d14018cad6e3eae67463
Kevin Villanova <kevillanova@gmail.com>
2023-05-17 23:40:37 -0300

    Atualização dos plugins Metabox

---
+++ wp/wp-content/plugins/mb-custom-post-type/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/mb-custom-post-type',
+        'pretty_version' => '2.5.4',
+        'version' => '2.5.4.0',
+        'reference' => 'f1abb3ca18a3831efc9c20e2e0be4b950906a7c7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'meta-box/support' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '21ac638244f63c9372ff9958810552a1a26135d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../meta-box/support',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'wpmetabox/mb-custom-post-type' => array(
+            'pretty_version' => '2.5.4',
+            'version' => '2.5.4.0',
+            'reference' => 'f1abb3ca18a3831efc9c20e2e0be4b950906a7c7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
