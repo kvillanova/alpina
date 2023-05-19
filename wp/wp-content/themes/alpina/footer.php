1d9aee9d7f7a02bda1f02ecbfc9a8ebfa87abafc
Kevin Villanova <kevillanova@gmail.com>
2023-05-19 08:26:50 -0300

    Início do footer

--- wp/wp-content/themes/alpina/footer.php
+++ wp/wp-content/themes/alpina/footer.php
@@ -1,3 +1,53 @@
+<footer class="py-5">
+    <nav class="container">
+
+        <div class="d-flex flex-column flex-lg-row ms-5 top-social justify-content-between align-items-center">
+            <img src="<?= get_template_directory_uri(); ?>/assets/svg/logo-branco-alpina.svg" alt="Alpina">
+            <div class="social d-flex flex-row justify-content-around py-3 py-lg-0">
+                <img class="mx-3" src="<?= get_template_directory_uri(); ?>/assets/svg/social/instagram.svg" alt="Instagram">
+                <img class="mx-3" src="<?= get_template_directory_uri(); ?>/assets/svg/social/linkedin.svg" alt="Instagram">
+                <img class="mx-3" src="<?= get_template_directory_uri(); ?>/assets/svg/social/whatsapp.svg" alt="WhatsApp">
+                <img class="mx-3" src="<?= get_template_directory_uri(); ?>/assets/svg/social/youtube.svg" alt="YouTube">
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-lg-row justify-content-between px-5 py-5 row">
+            <div class="d-flex flex-row justify-content-between">
+                
+                <div class="d-flex flex-column col-2 flex-grow-1">
+                    <a class="nav-link text-white" href="#">Início</a>
+                    <a class="nav-link text-white" href="#">Sobre</a>
+                    <a class="nav-link text-white" href="#">Soluções</a>
+                    <a class="nav-link text-white" href="#">Tecnologia</a>
+                    <a class="nav-link text-white" href="#">Marketing Digital</a>
+                    <a class="nav-link text-white" href="#">Cases</a>
+                    <a class="nav-link text-white" href="#">Aprenda</a>
+                    <a class="nav-link text-white" href="#">Contato</a>
+                </div>
+
+                <div class="d-flex flex-column flex-lg-row col-3 flex-grow-1 justify-content-between">
+                    <div class="d-flex flex-column flex-grow-1">
+                        <a class="nav-link text-white" href="#">Suporte</a>
+                        <a class="nav-link text-white" href="#">Chamados</a>
+                        <a class="nav-link text-white" href="#">E-mail</a>
+                        <a class="nav-link text-white" href="#">Chat Online</a>
+                    </div>
+                    <div class="d-flex flex-column flex-grow-1">
+                        <h4>Atendimento</h4>
+                        <a class="nav-link text-white" href="#">(54) 98358-4713</a>
+                        <a class="nav-link text-white" href="#">(54) 9999-9999</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+    </nav>
+</footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
