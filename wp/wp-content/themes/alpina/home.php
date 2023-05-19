c906a62f41db0f167264c0cf78e585ad2a2853bd
Kevin Villanova <kevillanova@gmail.com>
2023-05-19 11:25:38 -0300

    Add logos

--- wp/wp-content/themes/alpina/home.php
+++ wp/wp-content/themes/alpina/home.php
@@ -5,6 +5,8 @@ get_header();
 <?php 
 require_once __DIR__ . "/parts/home/carrossel.php"; 
 require_once __DIR__ . "/parts/home/solucoes.php"; 
+require_once __DIR__ . "/parts/home/topicos.php"; 
+require_once __DIR__ . "/parts/home/logos.php"; 
 ?>
 </main>
 <?php get_footer(); ?>
---
+++ wp/wp-content/themes/alpina/parts/home/logos.php
@@ -0,0 +1,12 @@
+<div id="logos" class="p-5 bg-white text-dark d-flex flex-column align-itens-center justify-content-center">
+    <p class="text-center">Não é sobre chegar ao topo,<br>
+        é sobre como e <strong>com quem você chega lá.</strong></p>
+    <div class="logos-container d-flex p-5 flex-nowrap overflow-hidden justify-content-center align-items-center">
+        <?php $glob = glob(get_template_directory() . '/assets/img/logo*.png');
+        $glob = array_map('basename', $glob);
+        ?>
+        <?php foreach($glob as $logo): ?>
+    <img class="mx-5" src="<?= get_template_directory_uri(); ?>/assets/img/<?=$logo?>" alt="Logo">
+    <?php endforeach; ?>
+    </div>
+</div>
