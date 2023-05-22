58d794a99ab8adfd862b84fbcd14693379c5025a
Kevin Villanova <kevillanova@gmail.com>
2023-05-22 09:17:23 -0300

    Adição do formulário

--- wp/wp-content/themes/alpina/home.php
+++ wp/wp-content/themes/alpina/home.php
@@ -7,6 +7,7 @@ require_once __DIR__ . "/parts/home/carrossel.php";
 require_once __DIR__ . "/parts/home/solucoes.php"; 
 require_once __DIR__ . "/parts/home/topicos.php"; 
 require_once __DIR__ . "/parts/home/logos.php"; 
+require_once __DIR__ . "/parts/home/form.php";
 ?>
 </main>
 <?php get_footer(); ?>
